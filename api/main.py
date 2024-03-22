13798d8525c1374ef53ace5261d3083b296166ad
Kyaw Paing Oo <82670447+Kyawpaingoo@users.noreply.github.com>
2024-03-22 23:37:51 +0630

    update

--- api/main.py
+++ api/main.py
@@ -1,102 +1,84 @@
 import urllib.parse
 import requests
+import os
 
 from datetime import datetime, timedelta
-from flask import Flask, redirect, request, jsonify, session
+from flask import Flask, redirect, request, jsonify, session, url_for
 
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
 
 app = Flask(__name__)
-app.secret_key = '4db482ba6ff8435689df84bd17109ed0'
+app.config['SECRET_KEY'] = os.urandom(64)
 
-CLIENT_ID = '2163cea1be834ab8be8e02cdeed00712'
-CLIENT_SECRET = '4db482ba6ff8435689df84bd17109ed0'
-REDIRECT_URI = 'http://127.0.0.1:5000/callback'
+CLIENT_ID = 'b4e5e4bb045240f59e63c7ccd0611ea1'
+CLIENT_SECRET = 'b3d68e9d44ad4a56a6b2af201c5144a2'
+REDIRECT_URI = 'http://localhost:5000/callback'
 
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
+SCOPE = 'user-library-read playlist-modify-private'
 
-@app.route('/')
-def index():
-    return "Welcome to Spotify <a href='/login'>Login</a>"
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope=SCOPE,
+    cache_handler=cache_handler,
+    show_dialog=True
+)
 
+sp = Spotify(auth_manager=sp_oauth)
 @app.route('/login')
 def login():
-    scope = 'user-read-private user-read-email'
-    
-    params = {
-        'client_id': CLIENT_ID,
-        'response_type': 'code',
-        'scope': scope,
-        'redirect_uri': REDIRECT_URI,
-        'show_dialog': True #set user to login again, set false or delete to save user login
-    }
-    
-    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
-    
-    return redirect(auth_url)
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        AUTH_URL = sp_oauth.get_authorize_url()
+        session["token_info"] = sp_oauth.get_cached_token()
+        return redirect(AUTH_URL)
+    return redirect(url_for('get_playlists'))
+
 
 @app.route('/callback')
 def callback():
-    if 'error' in request.args:
-        return jsonify({'error': request.args['error']})
+    token_info = sp_oauth.get_access_token(request.args['code'])
+    session["token_info"] = token_info
+    return redirect(url_for('get_playlists'))
     
-    if 'code' in request.args:
-        req_body={
-            'code': request.args['code'],
-            'grant_type': 'authorization_code',
-            'redirect_uri': REDIRECT_URI,
-            'client_id': CLIENT_ID,
-            'client_secrect': CLIENT_SECRET
-        }
-        
-        response = requests.post(TOKEN_URL, data=req_body)
-        token_info = response.json()
-        
-        session['access_token'] = token_info['access_token']
-        session['refresh_token'] = token_info['refresh_token']
-        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
-        
-        return redirect('/playlists')
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        AUTH_URL = sp_oauth.get_authorize_url()
+        return redirect(AUTH_URL)
     
-@app.route('/playlists')
-def get_playtlists():
-    if 'access_token' not in session:
-        return redirect('/login') #if expried, it redirects to login page
+    playlists = sp.current_user_playlists()
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
     
-    if datetime.now().timestamp() > session['expires_at']:
-        return redirect('/refresh-token') #if current time is greater than expire time it redirects to refresh-token url
+    return playlists_html
 
-    headers = {
-        'Authorization': f"Bearer {session['access_token']}"
-    }
-    
-    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers) #it response current login user playlist with access_token 
-    playlists = response.json() #playlist will be converted in to json format
-    
-    return jsonify(playlists)
 
-@app.route('/refresh-token')
-def refresh_token():
-    if 'refresh_token' not in session:
-        return redirect('/login') #if access token expires, redirect to login page
-    
-    if datetime.now().timestamp() > session['expires_at']:
-        req_body = {
-            'grant_type': 'refresh_token',
-            'refresh_token': session['refresh_token'],
-            'client_id': CLIENT_ID,
-            'client_secrect': CLIENT_SECRET
-        }
-        
-        response = requests.post(TOKEN_URL, data=req_body)
-        new_token_info = response.json()
-        
-        session['access_token'] = new_token_info['access_token']
-        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
-        
-        return redirect('/playlists')
-    
+@app.route('/generate')
+def generate_playlists():
+    token_info = session.get("token_info", None)
+    if not token_info:
+        return redirect("/login")
+
+    sp = Spotify(auth=token_info["access_token"])
+    user = sp.current_user()
+    print(f'Current User: {user}')
+    search_results = sp.search(q='Imagine Dragons', type='artist')
+    print(f'Search Result: {search_results['artists']['items']}')
+    # playlist = sp.user_playlist_create(user['id'], 'My Playlist', public=True)
+    # print(f'Playlist: {playlist['name']}')
+    return search_results
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
